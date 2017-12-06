ac15bb04c4d4f0b3ac7ba58f6f430b0ecd983b78
Rav Chandra <rc@braveface.nz>
2017-12-06 19:12:03 +1300

    day 4, part 2

--- 04/day04.ml
+++ 04/day04.ml
@@ -5,8 +5,6 @@ open ExtString
 
 let file = "input.txt"
 
-let part_1_input = "aa bb cc dd ee aa"
-
 module SS = Set.Make(String)
 
 let part_1 lines =
@@ -16,6 +14,18 @@ let part_1 lines =
     SS.cardinal the_set = List.length words in
   List.fold_left (fun acc x -> if x then acc + 1 else acc) 0 (List.map valid_passphrase lines)
 
+module SC = Set.Make(Char)
+module SSC = Set.Make(SC)
+
+let part_2 lines =
+  let valid_passphrase phrase =
+    let words = List.map String.strip (Str.split (Str.regexp " ") phrase) in
+    let make_word_set word = List.fold_right SC.add (String.explode word) SC.empty in
+    let the_set = List.fold_right SSC.add (List.map make_word_set words) SSC.empty in
+    SSC.cardinal the_set = List.length words in
+  List.fold_left (fun acc x -> if x then acc + 1 else acc) 0 (List.map valid_passphrase lines)
+
 let () =
   let raw_input = List.map String.strip (Std.input_list (open_in file)) in
   print_endline ("part 1: " ^ (string_of_int (part_1 raw_input))) ;
+  print_endline ("part 2: " ^ (string_of_int (part_2 raw_input))) ;
