662fdee89b161a15b0a636828518c5ab09460308
Rav Chandra <rc@braveface.nz>
2017-12-19 10:54:34 +1300

    day 18, part 1

--- 18/day18.ml
+++ 18/day18.ml
@@ -13,49 +13,48 @@ let reg_get_or_zero key m =
     | None -> 0
 
 type instruction =
-  Snd of string | Set of (string * int) | Add of (string * int) | Mul of (string * string) |
-  Mod of (string * int) | Rcv of string | Jgz of (string * int) | JgzR of (string * string)
+  Snd of (string * string) | Set of (string * string) | Add of (string * string) | Mul of (string * string) |
+  Mod of (string * string) | Rcv of (string * string) | Jgz of (string * string)
 
 let gen_instructions lst =
   let process instr =
     let pieces = String.split_on_char ' ' instr in
     match pieces with
-      | "snd" :: x :: [] -> Snd x
-      | "set" :: x :: n :: [] -> Set (x, int_of_string n)
-      | "add" :: x :: n :: [] -> Add (x, int_of_string n)
+      | "snd" :: x :: [] -> Snd (x, "nil")
+      | "set" :: x :: n :: [] -> Set (x, n)
+      | "add" :: x :: n :: [] -> Add (x, n)
       | "mul" :: x :: y :: [] -> Mul (x, y)
-      | "mod" :: x :: n :: [] -> Mod (x, int_of_string n)
-      | "rcv" :: x :: [] -> Rcv x
-      | "jgz" :: x :: n :: [] -> begin
-        try Jgz (x, int_of_string n)
-        with Failure _ -> JgzR (x, n)
-      end
+      | "mod" :: x :: n :: [] -> Mod (x, n)
+      | "rcv" :: x :: [] -> Rcv (x, "nil")
+      | "jgz" :: x :: n :: [] -> Jgz (x, n)
       | _ -> failwith "unknown?" in
   List.map process lst
 
 let print_instruction = function
-  | Snd x -> sprintf "snd(%s)" x
-  | Set (x, n) -> sprintf "set(%s)=>%d" x n
-  | Add (x, n) -> sprintf "add(%s)=>%d" x n
+  | Snd (x, _) -> sprintf "snd(%s)" x
+  | Set (x, n) -> sprintf "set(%s)=>%s" x n
+  | Add (x, n) -> sprintf "add(%s)=>%s" x n
   | Mul (x, y) -> sprintf "mul(%s)=>%s" x y
-  | Mod (x, n) -> sprintf "mod(%s)=>%d" x n
-  | Rcv x -> sprintf "rcv(%s)" x
-  | Jgz (x, n) -> sprintf "jgz(%s)=>%d" x n
-  | JgzR (x, y) -> sprintf "jgz REG(%s)=>%s" x y
+  | Mod (x, n) -> sprintf "mod(%s)=>%s" x n
+  | Rcv (x, _) -> sprintf "rcv(%s)" x
+  | Jgz (x, n) -> sprintf "jgz(%s)=>%s" x n
+
+let get_val_or_reg unk r =
+  try int_of_string unk
+  with Failure _ -> reg_get_or_zero unk r
 
 let process_instrunctions lst =
   let instructions = Array.of_list lst in
   let aux idx r =
     printf "idx=%d %s\n" idx (print_instruction instructions.(idx));
     match instructions.(idx) with
-      | Snd x -> 1, Registers.add "snd" (reg_get_or_zero x r) r
-      | Set (x, n) -> 1, Registers.add x n r
-      | Add (x, n) -> let curr = reg_get_or_zero x r in 1, Registers.add x (curr + n) r
-      | Mul (x, y) -> let curr = reg_get_or_zero x r in 1, Registers.add x (curr * (reg_get_or_zero y r)) r
-      | Mod (x, n) -> let curr = reg_get_or_zero x r in 1, Registers.add x (curr mod n) r
-      | Rcv x -> if (reg_get_or_zero x r) <> 0 then failwith (sprintf "rcv %d" (reg_get_or_zero "snd" r)) else (1, r)
-      | Jgz (x, n) -> if (reg_get_or_zero x r) > 0 then (n, r) else (1, r)
-      | JgzR (x, y) -> if (reg_get_or_zero x r) > 0 then ((reg_get_or_zero y r), r) else (1, r) in
+      | Snd (x, _) -> 1, Registers.add "snd" (get_val_or_reg x r) r
+      | Set (x, n) -> 1, Registers.add x (get_val_or_reg n r) r
+      | Add (x, n) -> let curr = reg_get_or_zero x r in 1, Registers.add x (curr + (get_val_or_reg n r)) r
+      | Mul (x, y) -> let curr = reg_get_or_zero x r in 1, Registers.add x (curr * (get_val_or_reg y r)) r
+      | Mod (x, n) -> let curr = reg_get_or_zero x r in 1, Registers.add x (curr mod (get_val_or_reg n r)) r
+      | Rcv (x, _) -> if (reg_get_or_zero x r) <> 0 then failwith (sprintf "rcv %d" (reg_get_or_zero "snd" r)) else (1, r)
+      | Jgz (x, n) -> if (reg_get_or_zero x r) > 0 then ((get_val_or_reg n r), r) else (1, r) in
   let rec go idx r =
     if idx < 0 || idx >= (List.length lst) then r
     else let step, r' = aux idx r in go (step + idx) r' in
